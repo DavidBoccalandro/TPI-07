239098bd467e9d63b6b0a47fc170a20cf81a102b
chimango <cristianhbassi@gmail.com>
2025-10-29 20:39:20 -0300

    primera fase 1 competencia 3

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include "jugar.h"
+#include <ctime>
+
+
+int main (){
+    srand(time(0));
+    int inicio=1;
+
+    if (inicio==1){
+    jugar();
+    };
+
+return 0;
+}
